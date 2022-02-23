09821a3e6c0e7a3277a15656dff7a208f99f5a1b
ubermanu <manu@paca.happy-dev.fr>
2022-02-23 12:37:30 +0100

    first commit

---
+++ rollup.config.js
@@ -0,0 +1,17 @@
+import pkg from './package.json'
+
+export default {
+  input: 'src/main.mjs',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+      sourcemap: true
+    },
+    {
+      file: pkg.module,
+      format: 'es',
+      sourcemap: true
+    }
+  ]
+}
