a6799d704b9b2788356d1c1926368d7f8652ca29
ubermanu <manu@paca.happy-dev.fr>
2022-10-06 23:07:49 +0200

    update build

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,24 @@
 import { terser } from 'rollup-plugin-terser'
 import pkg from './package.json'
 
-export default {
-  input: 'src/main.mjs',
-  output: [
-    {
-      file: pkg.main,
-      format: 'cjs',
-      sourcemap: true
-    },
-    {
-      file: pkg.module,
-      format: 'es',
-      sourcemap: true
-    }
-  ],
-  plugins: [terser()]
-}
+export default [
+  {
+    input: 'src/main.mjs',
+    output: [
+      {
+        file: pkg.main,
+        format: 'cjs',
+      },
+      {
+        file: pkg.module,
+        format: 'es',
+      },
+      {
+        file: pkg.unpkg,
+        format: 'umd',
+        name: 'arrosoir',
+      },
+    ],
+    plugins: [terser()],
+  },
+]
